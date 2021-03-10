0396cfa7800c757e7db6963dd352bac670dd702e
Turnerj <git@turnerj.com>
2021-03-10 18:47:21 +1030

    Initial test project with version details provider tests

---
+++ tests/TurnerSoftware.BuildVersioning.Tests/Tool/VersionDetailsProviderTests.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TurnerSoftware.BuildVersioning.Tool;
+
+namespace TurnerSoftware.BuildVersioning.Tests.Tool
+{
+	[TestClass]
+	public class VersionDetailsProviderTests
+	{
+		private static IEnumerable<object[]> GetVersionParsingData()
+		{
+			yield return new object[]
+			{
+				"abcdef",
+				new VersionDetails { CommitHash = "abcdef" }
+			};
+			yield return new object[]
+			{
+				"1.2.4-0-abcdef",
+				new VersionDetails { MajorVersion = 1, MinorVersion = 2, PatchVersion = 4, CommitHash = "abcdef", IsTaggedRelease = true }
+			};
+			yield return new object[]
+			{
+				"1.2.4-1-abcdef",
+				new VersionDetails { MajorVersion = 1, MinorVersion = 2, PatchVersion = 4, CommitHeight = 4, CommitHash = "abcdef" }
+			};
+			yield return new object[]
+			{
+				"1.2.4-alpha-4-abcdef",
+				new VersionDetails { MajorVersion = 1, MinorVersion = 2, PatchVersion = 4, PreRelease = "alpha", CommitHeight = 4, CommitHash = "abcdef" }
+			};
+			yield return new object[]
+			{
+				"1.2.4-alpha+build.123-4-abcdef",
+				new VersionDetails { MajorVersion = 1, MinorVersion = 2, PatchVersion = 4, PreRelease = "alpha", BuildMetadata = "build.123", CommitHeight = 4, CommitHash = "abcdef" }
+			};
+			yield return new object[]
+			{
+				"v1.2.4-alpha+build.123-4-abcdef",
+				new VersionDetails { MajorVersion = 1, MinorVersion = 2, PatchVersion = 4, PreRelease = "alpha", BuildMetadata = "build.123", CommitHeight = 4, CommitHash = "abcdef" }
+			};
+		}
+
+		[DataTestMethod]
+		[DynamicData(nameof(GetVersionParsingData), DynamicDataSourceType.Method)]
+		public void VersionParsing(string gitDescribeString, VersionDetails expected)
+		{
+			var commandRunnerMock = new Mock<IGitCommandRunner>();
+			commandRunnerMock.Setup(c => c.GitDescribe()).Returns(gitDescribeString);
+			var versionDetailsProvider = new VersionDetailsProvider(commandRunnerMock.Object);
+
+			var result = versionDetailsProvider.GetVersionDetails();
+			Assert.AreEqual(expected, result);
+		}
+	}
+}
